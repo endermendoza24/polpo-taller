89527574765935eaeb0ba26a0404dd8e10bb013b
Endersson Mendoza <enderalonso24@gmail.com>
2021-02-06 20:33:58 -0600

    Interfaz creada, todavia sin funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Navigation from './componentes/navigation';
+import {todos} from './todos.json';
 
-function App() {
+
+const fecha = () =>{
+  
+  var d = new Date();
+  return d.getDate() + ' de ' + d.getMonth() + 'del año ' + d.getFullYear()  ;
+
+  
+}
+
+
+class App extends Component{
+  constructor(){
+  super();
+  this.state = {
+    todos
+  }
+}
+
+
+render(){
+  const todos = this.state.todos.map((todos,i) =>{
+    return(
+      
+      <div className="col-md-4">
+        <div className="card mt-4">
+       <div className="card-header">
+       <h3>{todos.titulo}</h3>
+       <span className="badge badge-pill badge-danger ml-2">
+             {todos.Prioridad}
+           </span>
+        
+         </div>
+        
+           
+         
+
+         <div className="card-body">
+           <p>{todos.Contenido}</p>
+           <p><strong>{todos.Autor}</strong></p>
+         </div>
+
+         <div>
+           {
+             fecha() 
+           }
+         </div>
+
+
+
+         </div>
+      </div>
+         
+      
+    )
+  }
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav className="navbar navbar-dark bg-dark">
+        <h4>
+        <a href="#" className="text-white">
+         Tareas
+         <span className="badge badge-pill badge-warning ml-2">
+           {this.state.todos.length}
+         </span>
         </a>
-      </header>
+        </h4>
+      </nav>
+
+    <div className="container">
+      <div className="row mt-4">
+      {todos}
+      </div>
+    </div>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/componentes/navigation.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Navigation extends Component{
+    render(){
+        return(
+            <nav className="navbar navbar-dark bg-dark">
+                <a href="www.google.com" className="text-white">
+                   
+                </a>
+            </nav>
+        )
+    }
+}
+export default Navigation;
